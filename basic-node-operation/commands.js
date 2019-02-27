054c07500bd028893a03270488ce68500e95c186
grace-ko <gracekodes@gmail.com>
2019-02-26 23:30:39 -0800

    added more commands

--- basic-node-operation/commands.js
+++ basic-node-operation/commands.js
@@ -8,10 +8,28 @@ function done(output) {
 function evaluateCmd(userInput) {
   const userInputArray = userInput.split(" ");
   const command = userInputArray[0];
-  console.log(command);
+  done(command);
+  switch (command) {
+    case "echo":
+      commandLibrary.echo(userInputArray.slice(1).join(" "));
+      break;
+    case "cat":
+      commandLibrary.cat(userInputArray.slice(1));
+      break;
+  }
 }
 
 const commandLibrary = {
+  "echo": function(userInput) {
+    done(userInput);
+  },
+  "cat": function(fullPath) {
+    const fileName = fullPath[0];
+    fs.readFile(fileName, (err, data) => {
+      if (err) throw err;
+      done(data);
+    });
+  }
 };
 
 module.exports.commandLibrary = commandLibrary;
